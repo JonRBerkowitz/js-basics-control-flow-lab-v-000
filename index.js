505f4b01cfcdf599f2590b7f04b0c3e355b00f41
Jonathan Berkowitz <jonrberkowitz@gmail.com>
2019-01-30 19:02:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 // Write your code in this file!
 function scuberGreetingForFeet(feet) {
   if (feet < 400) {
-    result = 'This one is on me!'
+    result = 'This one is on me!';
   } else if (feet > 2500) {
-    result = 'No can do.'
+    result = 'No can do.';
   } else if (feet > 2000) {
-    result = 'I will gladly take your thirty bucks.'
+    result = 'I will gladly take your thirty bucks.';
   }
+  return result
+}
+
+function ternaryCheckCity(city) {
+  let result;
+  city === 'NYC' ? (result = 'Ok, sounds good.') : (result = 'No go.');
   return result
 }
