81e1f2870787b222392215d79061cc302a06d27d
Jonathan Berkowitz <jonrberkowitz@gmail.com>
2019-01-30 18:59:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
 function scuberGreetingForFeet(feet) {
   if (feet < 400) {
     result = 'This one is on me!'
-  } else if (feet > 2000) {
-    result = 'I will gladly take your thirty bucks.'
   } else if (feet > 2500) {
     result = 'No can do.'
+  } else if (feet > 2000) {
+    result = 'I will gladly take your thirty bucks.'
   }
   return result
 }
